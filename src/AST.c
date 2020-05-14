fb2b12aedce66ae9cfaffedf00f596064da0aaa6
Johann150 <20990607+Johann150@users.noreply.github.com>
2020-05-14 08:35:09 +0200

    replace (void*) 0 with NULL
    What was the cast to void* supposed to do?

--- src/AST.c
+++ src/AST.c
@@ -1,36 +1,36 @@
+#include<stdlib.h>
 #include "include/AST.h"
-
 
 AST_T* init_ast(int type)
 {
     AST_T* ast = calloc(1, sizeof(struct AST_STRUCT));
     ast->type = type;
 
-    ast->scope = (void*) 0;
+    ast->scope = NULL;
 
     /* AST_VARIABLE_DEFINITION */
-    ast->variable_definition_variable_name = (void*) 0;
-    ast->variable_definition_value = (void*) 0;
+    ast->variable_definition_variable_name = NULL;
+    ast->variable_definition_value = NULL;
 
     /* AST_FUNCTION_DEFINITION */
-    ast->function_definition_body = (void*) 0;
-    ast->function_definition_name = (void*) 0;
-    ast->function_definition_args = (void*) 0;
+    ast->function_definition_body = NULL;
+    ast->function_definition_name = NULL;
+    ast->function_definition_args = NULL;
     ast->function_definition_args_size = 0;
 
     /* AST_VARIABLE */
-    ast->variable_name = (void*) 0;
+    ast->variable_name = NULL;
 
     /* AST_FUNCTION_CALL */
-    ast->function_call_name = (void*) 0;
-    ast->function_call_arguments = (void*) 0;
+    ast->function_call_name = NULL;
+    ast->function_call_arguments = NULL;
     ast->function_call_arguments_size = 0;
 
     /* AST_STRING */
-    ast->string_value = (void*) 0;
+    ast->string_value = NULL;
 
     /* AST_COMPOUND */
-    ast->compound_value = (void*) 0;
+    ast->compound_value = NULL;
     ast->compound_size = 0;
 
     return ast;
