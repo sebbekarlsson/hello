28ba77e9922a7f4d49d21515a5340be27750eaa1
isaac025 <isaac.lopez@upr.edu>
2020-09-30 02:05:47 -0400

    Added prompt, added mutliple file and check if valid file

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 #include "include/lexer.h"
 #include "include/parser.h"
 #include "include/visitor.h"
 #include "include/io.h"
 
-
+#define MAX_LIMIT 20
 void print_help()
 {
     printf("Usage:\nhello.out <filename>\n");
@@ -13,17 +14,36 @@ void print_help()
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2)
-        print_help();
+    if (argc >= 2){
+        for(int i = 1; i < argc; i++){
+            int len = strlen(argv[i]);
+            char* last_four = &argv[i][len-6];
+            if(strcmp(last_four,".hello") == 0){
+                                lexer_T* lexer = init_lexer(
+                    get_file_contents(argv[i])
+                );
+                parser_T* parser = init_parser(lexer);
+                AST_T* root = parser_parse(parser, parser->scope);
+                visitor_T* visitor = init_visitor();
+                visitor_visit(visitor, root);
+            }
 
-    lexer_T* lexer = init_lexer(
-        get_file_contents(argv[1])
-    );
-
-    parser_T* parser = init_parser(lexer);
-    AST_T* root = parser_parse(parser, parser->scope);
-    visitor_T* visitor = init_visitor();
-    visitor_visit(visitor, root);
-
+            else {
+                print_help();
+            }
+        }
+    }
+    else {
+        char input[MAX_LIMIT];
+        while(1){
+            printf(">>> ");
+            fgets(input,MAX_LIMIT, stdin);
+            lexer_T* lexer = init_lexer(input);
+            parser_T* parser = init_parser(lexer);
+            AST_T* root = parser_parse(parser, parser->scope);
+            visitor_T* visitor = init_visitor();
+            visitor_visit(visitor, root);
+        }
+    }
     return 0;
 }
